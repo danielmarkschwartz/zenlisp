32b61f5a89eb357e5cc4c8b52169ad299ec28967
Daniel Schwartz <mrdschwartz@gmail.com>
2020-07-11 08:09:03 -0700

    Add -v flag to main() command line args

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "common.h"
 #include "parse.h"
 
 int main(int argc, char **argv) {
-    printf("ZenLisp " ZENLISP_VER "\n");
+    if(argc == 2 && strcmp(argv[1], "-v") == 0) {
+        printf("ZenLisp " ZENLISP_VER "\n");
+        return 0;
+    }
 
     if(argc > 2) {
         fprintf(stderr, "ERROR: Expected at most 1 argument\n");
